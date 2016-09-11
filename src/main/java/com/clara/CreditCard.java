283e0ead03292c66a455c90d8adb92c77b1a26c6
miked <whatthafunk3@aol.com>
2016-09-10 23:29:19 -0500

    updated

--- src/main/java/com/clara/CreditCard.java
+++ src/main/java/com/clara/CreditCard.java
@@ -14,7 +14,9 @@ public class CreditCard {
         //Ask user for credit card number. store number as a String.
         System.out.println("Please enter the credit card number, digits only:");
         String ccNumber = stringScanner.nextLine();
-        boolean isValid = isValidCreditCard(ccNumber);
+        char checkNumber = ccNumber.charAt((ccNumber.length()-1));
+
+        boolean isValid = isValidCreditCard(ccNumber, checkNumber);
 
         if (isValid) {
             System.out.println("This seems to be a valid credit card number");
@@ -25,12 +27,48 @@ public class CreditCard {
         stringScanner.close();
     }
 
-    public static boolean isValidCreditCard(String cc) {
+    public static boolean isValidCreditCard(String cc, char cn) {
+        String card = cc;
+        String checkString = "" + cn;
+        int check = Integer.valueOf(checkString);
+        card = card.substring(0, ( card.length() -1));
+        String cardReverse = new StringBuilder(card).reverse().toString();
 
-        //TODO Replace with your code to process the credit card number, and determine if it is valid.
-        //TODO Make sure all the tests pass!
+        char [] ccArray = cardReverse.toCharArray();
+        int [] isWorking = new int[ccArray.length];
+        int sumOfDigits = 0;
 
-        return false;
+        for(int i=0; i<ccArray.length; i+=2)
+        {
+            isWorking[i] = Character.getNumericValue( ccArray[i]);
+        }
+        for (int j=0; j<isWorking.length; j++)
+        {
+            if ( (j % 2)==0)
+            {
+                isWorking[j] = isWorking[j]*2;
+            }
+        }
+        for (int k=0; 1<isWorking.length; k++)
+        {
+            if (isWorking[k]>9)
+            {
+                isWorking[k] = isWorking[k]-9;
+            }
+        }
+        for (int l = 0; l < isWorking.length; l++)
+        {
+            sumOfDigits+= isWorking[l];
+        }
+        if(sumOfDigits % 10 == check)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
 
     }
 
